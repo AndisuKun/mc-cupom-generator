be803998d02bc9cb2b2b996dc861c558815d7346
Anderson Viana <anderson@offertech.com.br>
2021-06-15 22:04:34 -0300

    testes e teste

--- main.py
+++ main.py
@@ -2,16 +2,14 @@ import requests
 
 print(f'escolha a url {url} ou a {url2}')
 escolha = input(int)
-if escolha = 1:
-    
+if (escolha==1):
     def import_url():
         url = requests.get('https://mcexperiencia.com')
         # print(url[0])
-        print(url.encoding)
-        return
-
-def import_url2():
-    url2 = requests.get('https://www.mcexperienciasurvey.com/Index.aspx?c=065819')
-    # print(url[0])
-    print(url2.encoding)
-    return
+        return print(url.encoding)
+else:
+    def import_url2():
+        url2 = requests.get('https://www.mcexperienciasurvey.com/Index.aspx?c=065819')
+        # print(url[0])
+        return print(url2.encoding)
+        
