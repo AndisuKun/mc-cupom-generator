b172ecbb92211383726e6530b28e5866704f47f6
Anderson Viana <anderson@offertech.com.br>
2021-06-15 22:00:10 -0300

    update

---
+++ main.py
@@ -0,0 +1,17 @@
+import requests
+
+print(f'escolha a url {url} ou a {url2}')
+escolha = input(int)
+if escolha = 1:
+    
+    def import_url():
+        url = requests.get('https://mcexperiencia.com')
+        # print(url[0])
+        print(url.encoding)
+        return
+
+def import_url2():
+    url2 = requests.get('https://www.mcexperienciasurvey.com/Index.aspx?c=065819')
+    # print(url[0])
+    print(url2.encoding)
+    return
